231701139cb2d8827f4877438e4513fc70f33a63
John Wohlbier <jgwohlbier@sei.cmu.edu>
2019-10-03 10:15:50 -0400

    fix comments to account for position switching of args to foo

--- 2_main_foo.cpp
+++ 2_main_foo.cpp
@@ -17,10 +17,10 @@ int main(int argc, char* argv[])
 // foo(int, int):
 //         pushq   %rbp            // push calling frame bp qw onto stack
 //         movq    %rsp, %rbp      // move top of stack qw ptr into %rbp
-//         movl    %edi, -4(%rbp)  // move val in %edi (b) to 4 bytes below sp
-//         movl    %esi, -8(%rbp)  // move val in %esi (a) to 8 bytes below sp
-//         movl    -4(%rbp), %edx  // move val at 4 bytes below bp to %edx
-//         movl    -8(%rbp), %eax  // move val at 4 bytes below bp to %eax
+//         movl    %edi, -4(%rbp)  // move val in %edi (a) to 4 bytes below sp
+//         movl    %esi, -8(%rbp)  // move val in %esi (b) to 8 bytes below sp
+//         movl    -4(%rbp), %edx  // move val at 4 bytes below bp to %edx (a)
+//         movl    -8(%rbp), %eax  // move val at 4 bytes below bp to %eax (b)
 //         addl    %edx, %eax      // add values in %edx %eax, leave in %eax
 //         popq    %rbp            // pop calling base pointer qw from stack
 //         ret                     // return
@@ -30,12 +30,12 @@ int main(int argc, char* argv[])
 //         subq    $32, %rsp       // allocate 32 bytes of stack space
 //         movl    %edi, -20(%rbp) // move argv to 20 bytes below bp
 //         movq    %rsi, -32(%rbp) // move argc to 32 bytes below bp
-//         movl    $3, -8(%rbp)    // put 3 into 8 bytes below bp
-//         movl    $2, -4(%rbp)    // put 2 into 4 bytes below bp
-//         movl    -4(%rbp), %edx  // move val 4 bytes below bp to %edx
-//         movl    -8(%rbp), %eax  // move val 8 bytes below bp to %eax
-//         movl    %edx, %esi      // move val in %edx to function arg %esi
-//         movl    %eax, %edi      // move val in %eax to function arg %edi
+//         movl    $3, -8(%rbp)    // put 3 (a) into 8 bytes below bp
+//         movl    $2, -4(%rbp)    // put 2 (b) into 4 bytes below bp
+//         movl    -4(%rbp), %edx  // move val 4 bytes below bp to %edx (b)
+//         movl    -8(%rbp), %eax  // move val 8 bytes below bp to %eax (a)
+//         movl    %edx, %esi      // move val in %edx to function arg %esi (b)
+//         movl    %eax, %edi      // move val in %eax to function arg %edi (a)
 //         call    foo(int, int)   // call foo
 //         leave                   // set %rsp to %rbp, pop top stack into %rbp
 //         ret                     // return
@@ -51,3 +51,5 @@ int main(int argc, char* argv[])
 
 // stack frame created in main, %rbp != %rsp. Not sure why argc, argv
 // are put in the frame.
+
+// note that a, b change relative positions wrt %rbp in foo
